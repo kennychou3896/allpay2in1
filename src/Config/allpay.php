fb24a7f420288f62aebfb8c361e68978b32d0d3f
kennychou3896 <ken3896@mail2000.com.tw>
2017-07-07 08:33:42 +0800

    Update allpay.php

--- src/Config/allpay.php
+++ src/Config/allpay.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'ServiceURL' => 'http://payment-stage.allpay.com.tw/Cashier/AioCheckOut',
+    'ServiceURL' => 'http://payment-stage.ecpay.com.tw/Cashier/AioCheckOut',
     'HashKey'    => '5294y06JbISpM5x9',
     'HashIV'     => 'v77hoKGq4kWxNNIS',
     'MerchantID' => '2000132',
