f53883d26a6cd7a3b032d23e9ed0e330e0a7845c
kennychou3896 <ken3896@mail2000.com.tw>
2017-07-07 11:24:01 +0800

    Update routes.php

--- src/routes.php
+++ src/routes.php
@@ -1,7 +1,7 @@
 <?php
 
 Route::group([
-    'namespace' => 'Kennychou3896\Allpay\Controllers',
+    'namespace' => 'Kennychou3896\Allpay2in1\Controllers',
     'prefix'    => 'allpay_demo'],
     function () {
         Route::get('/', 'DemoController@index');
